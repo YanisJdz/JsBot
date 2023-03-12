50e7e8883d48b96a2dd38a3b57120adb18818489
Yanis <yanisjdz@outlook.fr>
2023-03-12 15:05:34 +0100

    Add Zak Pig for Spipe

--- src/events/feur.js
+++ src/events/feur.js
@@ -14,8 +14,15 @@ module.exports = {
     name: Events.MessageCreate,
     async execute(message){
         if (message.content.toLowerCase().includes("quoi") && !message.author.bot) {
-            message.reply(feurs[Math.floor(Math.random() * feurs.length)])
-                .then(() => console.log(`Replied to message "${message.content}"`))
-                .catch(console.error);        }
+            if (message.content.toLowerCase().includes("zak") && message.content.toLowerCase().includes("?") && message.author.id == 260079309434257413) {
+                message.reply("Il mange, il dort, il est un pig :pig:")
+                    .then(() => console.log(`Zak le pig: "${message.content}"`))
+                    .catch(console.error);
+                }else{
+                    message.reply(feurs[Math.floor(Math.random() * feurs.length)])
+                    .then(() => console.log(`Réponse feur: "${message.content}"`))
+                    .catch(console.error);
+                }
+            }
     },
 }
